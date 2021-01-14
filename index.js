98a4254e6353f5cd49033f0d02ec55adb75cb892
Rebecca Sorrell <rebeccassorrell@gmail.com>
2021-01-14 23:43:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector) {
+  var element = document.querySelector(selector)
+  return element
+}
 
+function nestedTarget() {
+  var target = document.querySelector('.target')
+  return target
+}
+
+function increaseRankBy(n) {
+  var mainApp = document.querySelectorAll('.ranked-list li')
+  console.log(mainApp)
+  for (var i =0; i < mainApp.length; i++) {
+    var element = mainApp[i]
+    var rankedLi = parseInt(element.innerHTML) + n
+    element.innerHTML = rankedLi
+  }
+}
+
+function deepestChild() {
+  var currentChild = document.querySelectorAll('#grand-node')[0]
+  var lastChild = []
+  while (currentChild !== null) {
+    lastChild = currentChild
+    currentChild = currentChild.querySelector(':first-child')
+  }
+  return lastChild
+}
